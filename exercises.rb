ba0a5aaca642187bb3089e3cf37fb87af608055f
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-16 14:55:07 -0400

    first commit

---
+++ exercises.rb
@@ -0,0 +1,18 @@
+colours = ["blue", "purple", "yellow"]
+ages = [24, 29, 30, 30]
+coin = ["tails", "heads", "heads", "tails", "heads"]
+artists = ["krill", "outkast", "fugazi"]
+colours = [:blue, :purple, :yellow]
+
+words = {
+  :crass => "lacking sensitivity,"
+  :fried => "cooked in oil,"
+  :roundabout => "road junction,"
+}
+
+films = {
+  :wild_strawberries => 1957,
+  :the_naked_gun => 1988,
+  :playtime => 1967,
+}
+puts films
