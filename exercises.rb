1dfa3c062d33db2a7c8e0e0dee6bc1a22519fe18
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-16 15:02:17 -0400

    exercise 0 complete

--- exercises.rb
+++ exercises.rb
@@ -1,13 +1,13 @@
 colours = ["blue", "purple", "yellow"]
-ages = [24, 29, 30, 30]
+ages = [24, 29, 30, 28, 30]
 coin = ["tails", "heads", "heads", "tails", "heads"]
 artists = ["krill", "outkast", "fugazi"]
 colours = [:blue, :purple, :yellow]
 
 words = {
-  :crass => "lacking sensitivity,"
-  :fried => "cooked in oil,"
-  :roundabout => "road junction,"
+  :crass => "lacking sensitivity",
+  :fried => "cooked in oil",
+  :roundabout => "road junction"
 }
 
 films = {
@@ -15,4 +15,18 @@ films = {
   :the_naked_gun => 1988,
   :playtime => 1967,
 }
-puts films
+
+cities = {
+  :toronto => "2.8 million",
+  :chicago => "2.7 million",
+  :new_york => "8.4 million"
+}
+
+names_and_ages = {
+  :emile => 29,
+  :nabeel => 30,
+  :zach => 28,
+  :vanessa => 30
+}
+
+# puts films, cities, names_and_ages
